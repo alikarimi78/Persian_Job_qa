0fed612c628d28595f948c464e3f4c5acbc03bcb
a.karimi <ali0karimi1azari@gmail.com>
2026-07-04 20:23:02 +0330

    fix: load data from env

--- phase1_new_project.py
+++ phase1_new_project.py
@@ -45,6 +45,10 @@ import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 try:
     import torch
     _HAS_CUDA = torch.cuda.is_available()
